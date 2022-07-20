b254d8672c819655087bfdaf643834d73565c317
Ruslan <81380652+SomaSan2005@users.noreply.github.com>
2022-07-20 14:31:53 +0200

    Create Max tra Due Numeri.py

---
+++ Max tra Due Numeri.py
@@ -0,0 +1,13 @@
+def my_max(a, b):
+
+    if a == b:
+
+        print("I numeri sono identici")
+
+    elif a > b:
+
+        print("Il numero più grande tra i due è " + str(a))
+
+    else:
+
+        print("Il numero più grande tra i due è " + str(b))
